a7ead02ad37f5a45c400fdfc29a3a90305290ab0
YeeSuan95 <yixuan_ong@hotmail.com>
2017-10-28 01:51:42 +0800

    Fixed creditCardValidate.php

--- creditCardValidate.php
+++ creditCardValidate.php
@@ -1,4 +1,5 @@
 <?php
+
 include "qrcodelib/qrlib.php";
 
 
@@ -48,7 +49,7 @@ if (isset($_POST['submit'])) {
         $_SESSION['CCE'] = 'Credit Card Expiry date is Empty!';
         $okay = FALSE;
         header("Location: Payment2.php");
-    } 
+    }
     if (empty($_POST['CVV2'])) {
         $_SESSION['CVV2'] = 'CVV2 is Empty!';
         $okay = FALSE;
@@ -78,6 +79,22 @@ if (isset($_POST['submit'])) {
             //echo '<br>';
             //echo 'show id: '. $_SESSION['show_id'];
             //echo '<br>';
+            $userid = $_SESSION['user'];
+            $CCNum = $_POST['CreditCardNo'];
+            $CCName = $_POST['CreditCardName'];
+            $date = $_SESSION['BookTime'];
+            $timeType = explode(" ", $date);
+            $timeItems = explode(":", $timeType[0]);
+
+            $Formattime = implode(":", $timeItems);
+            $Formattime .= ":00";
+            $Formaydate = $_SESSION['BookDate'];
+
+            // TICKET COLLECT
+            $sql_queryticketCollect = $MySQLiconn->query("INSERT INTO ticketcollection( ticket_collected, booking_time, CreditCardNum, CreditCardName, user_id, booking_date) VALUES (0, '$Formattime', '$CCNum', '$CCName', '$userid', '$Formaydate')");
+            mysqli_query($MySQLiconn, $sql_queryticketCollect);
+            $collectionresult = mysqli_affected_rows($MySQLiconn);
+            $id = mysqli_insert_id($MySQLiconn);
             foreach ($_SESSION['check_list'] as $seat) {
                 //echo '<br>';
                 $row = $dic[$seat[0]];
@@ -90,77 +107,68 @@ if (isset($_POST['submit'])) {
                 //echo '<br>';
                 $showInfoID = $_SESSION['show_id'];
                 $movieID = $_SESSION['movie_id'];
-                $userid = $_SESSION['user'];
-                //echo 'User id '.$_SESSION['user'];
-                //echo 'show info id: '. $showInfoID;
-                //echo 'show movie id: '. $movieID;
-                $CCNum = $_POST['CreditCardNo'];
-                $CCName = $_POST['CreditCardName'];
-                $date = $_SESSION['BookTime'];
-                $timeType = explode(" ", $date);
-                $timeItems = explode(":", $timeType[0]);
-                if($timeType[1] == "PM"){
-                    $timeItems[0] += 12;
-                }
-                $Formattime = implode(":", $timeItems);
-                $Formattime .= ":00";
-                $Formaydate = $_SESSION['BookDate'];
-                
-                $sql_queryticketCollect = $MySQLiconn->query("INSERT INTO ticketcollection( ticket_collected, booking_time, CreditCardNum, CreditCardName, user_id, booking_date) VALUES (0, '$Formattime', '$CCNum', '$CCName', '$userid', '$Formaydate')");
-                mysqli_query($MySQLiconn, $sql_queryticketCollect);
-                $id = mysqli_insert_id($MySQLiconn);
-                                
+
+                // BOOKING
                 $sql_querybooking = $MySQLiconn->query("INSERT INTO booking( showInfo_id, seat_no, showInfo_row, showInfo_column, movie_id, collection_id) VALUES ('$showInfoID','$seat','$row','$col', '$movieID', '$id')");
                 mysqli_query($MySQLiconn, $sql_querybooking);
-                
-                //echo $result;
+
+                // QR CODE
+                $randmd = md5(uniqid(rand(), true));
+                $fixedvalue = 123456;
+                $qrcode = (string) ($fixedvalue . $randmd . $_SESSION['user'] . $_SESSION['show_id'] . $_SESSION['name']);                //qrcode making unencrypted
+                $hashedfile = hash("sha256", $qrcode);
+                $fileurl = 'Theipaddress/checkQRCode.php?qrCode="' . $hashedfile . '"';
+                $sql_qradd = $MySQLiconn->query("UPDATE ticketcollection SET qrValue='$hashedfile' WHERE collection_id='$id'");
+                /* $collectid = mysqli_query($MySQLiconn, "SELECT collection_id FROM ticketcollection WHERE user_id ='". $userid. "'" );
+                  echo $showtheid; //the problem is here
+                 */
+                //$sql_qradd = $MySQLiconn->query("UPDATE ticketcollection SET qrValue='$hashedfile' WHERE collection_id=22");
+                //$sql_qradd = $MySQLiconn->query("INSERT INTO ticketcollection(qrValue) VALUES ('$hashedfile') WHERE (collection_id='".$collectid. "'");
+                mysqli_query($MySQLiconn, $sql_qradd);
             }
-
-            $result = mysqli_query($MySQLiconn, "SELECT * FROM `showinfo` WHERE showInfo_id ='" . $_SESSION['show_id'] . "'");
-            $showinfo = mysqli_fetch_assoc($result);
-            $result2 = mysqli_query($MySQLiconn, "SELECT * FROM `movie` WHERE movie_id ='" . $showinfo['movie_id'] . "'");
-            $movie = mysqli_fetch_assoc($result2);
-			$randmd = md5(uniqid(rand(), true));
-			$fixedvalue = 123456;
-			$qrcode = (string)($fixedvalue.$randmd.$_SESSION['user'].$_SESSION['show_id']. $_SESSION['name']) ;                //qrcode making unencrypted
-			$tempDir = 'qrcodelib/image/';
-			$hashedfile = hash("sha256", $qrcode);
-			$fileurl = '/checkQRCode.php?qrCode="'.$hashedfile.'"';
-
+            
             // EMAIL
-            require 'email/PHPMailerAutoload.php';
+            if ($sql_querybooking && $sql_queryticketCollect) {
+                $id = mysqli_insert_id($MySQLiconn);
+                $result = mysqli_query($MySQLiconn, "SELECT * FROM `showinfo` WHERE showInfo_id ='" . $_SESSION['show_id'] . "'");
+                $showinfo = mysqli_fetch_assoc($result);
+                $result2 = mysqli_query($MySQLiconn, "SELECT * FROM `movie` WHERE movie_id ='" . $showinfo['movie_id'] . "'");
+                $movie = mysqli_fetch_assoc($result2);
+               
+                require 'email/PHPMailerAutoload.php';
 
-            $mail = new PHPMailer;
-            $mail->isSMTP();                                      // Set mailer to use SMTP
-            $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
-            $mail->SMTPAuth = true;                               // Enable SMTP authentication
-            $mail->Username = '3103.3203.team15@gmail.com';       // SMTP username
-            $mail->Password = 'te@m15ssd';                        // SMTP password
-            $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-            $mail->Port = 587;                                    // TCP port to connect to
+                $mail = new PHPMailer;
+                $mail->isSMTP();                                      // Set mailer to use SMTP
+                $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
+                $mail->SMTPAuth = true;                               // Enable SMTP authentication
+                $mail->Username = '3103.3203.team15@gmail.com';       // SMTP username
+                $mail->Password = 'te@m15ssd';                        // SMTP password
+                $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+                $mail->Port = 587;                                    // TCP port to connect to
 
-            $mail->setFrom('from@example.com', 'Golden Village');
-            $mail->addAddress('' . $_SESSION['email'] . '');      // Add a recipient
+                $mail->setFrom('from@example.com', 'Golden Village');
+                $mail->addAddress('' . $_SESSION['email'] . '');      // Add a recipient
 
-            $mail->isHTML(true);                                  // Set email format to HTML
+                $mail->isHTML(true);                                  // Set email format to HTML
 
-            $mail->Subject = '' . $movie['movie_name'] . ' seats successfully booked!';
-            $mail->Body = '<p>Dear ' . $_SESSION['name'] . ',</p>
+                $mail->Subject = '' . $movie['movie_name'] . ' seats successfully booked!';
+                $mail->Body = '<p>Dear ' . $_SESSION['name'] . ',</p>
                                     <p>Please be informed that your transaction is confirmed and payment has been debited from your account.</p>
                                     <p><b><u>Movie Ticket Details</u></b>
                                     <p>Booked Date: ' . date("d-m-y", strtotime($showinfo['showInfo_date'])) . '</p> 
                                     <p>Booked Time: ' . $showinfo['showInfo_time'] . '</p>
                                     <p>Your seat(s) is/are ' . implode(', ', $_SESSION['check_list']) . ' with a total price of $' . $_SESSION['price'] . '</p>
-									<p>our QR : <img src= https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=' . $fileurl .'></p>';
-            if (!$mail->send()) {
-                echo 'Movie tickets details could not be sent.';
-                echo 'Mailer Error: ' . $mail->ErrorInfo;
-            } else {
-                echo '<script language="javascript">';
-                echo 'alert("Success! We will be sending an email to you shortly");';
-                echo 'window.location.href = "Success.php";';
-                echo '</script>';
-                $_SESSION['message2'] = 'Success! We will be sending an email to you shortly!';
+									<p>our QR : <img src= https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=' . $fileurl . '></p>';
+                if (!$mail->send()) {
+                    echo 'Movie tickets details could not be sent.';
+                    echo 'Mailer Error: ' . $mail->ErrorInfo;
+                } else {
+                    echo '<script language="javascript">';
+                    echo 'alert("Success! We will be sending an email to you shortly");';
+                    echo 'window.location.href = "Success.php";';
+                    echo '</script>';
+                    $_SESSION['message2'] = 'Success! We will be sending an email to you shortly!';
+                }
             }
             //header("Location: Success.php");
         } else {
