bfc808755e70f2f0ad2c5a8a7d76c00b1aa2edc8
LeeJunWei951 <junwei.lee_2015@sit.singaporetech.edu.sg>
2017-10-27 13:30:24 +0800

    Edit QR Checker to remove special characters

--- checkQRCode.php
+++ checkQRCode.php
@@ -1,21 +1,25 @@
 <?php
+
 session_start();
 include_once 'dbconnect.php';
 
 if (!isset($_GET['qrCode'])) {
     header("Location: index.php");
 }
-$res = mysqli_query($MySQLiconn, "SELECT * FROM ticketcollection WHERE qrValue=".$_GET['qrCode']);
-$res = mysqli_query($MySQLiconn, "UPDATE ticketcollection SET ticket_collected=1 WHERE qrValue=".$_GET['qrCode']);
-$row = mysqli_affected_rows($res);
-if($rows==1){
-    echo "Tickets redeemed successfully";
+
+$qrValue = mysqli_real_escape_string($MySQLiconn, $_GET['qrCode']);
+
+$res = mysqli_query($MySQLiconn, "SELECT * FROM ticketcollection WHERE qrValue=" . $qrValue);
+if (mysqli_num_rows($res) == 1) {
+    $res = mysqli_query($MySQLiconn, "UPDATE ticketcollection SET ticket_collected=1 WHERE qrValue=" . $qrValue);
+    $row = mysqli_affected_rows($res);
+    if ($rows == 1) {
+        echo "Tickets redeemed successfully";
+    } else {
+        echo "Error redeeming ticket";
+    }
 }
-else{
-    echo "Error redeeming ticket";
-}
-
-echo 'QR Code is '. $_GET['qrCode'];
 
 
+echo 'QR Code is ' . $qrValue;
 ?>
