d76685fc41a2e537f8fc179bb9465abfa0b3c5d4
MunHanTeng <MunHan.Teng_2015@SingaporeTech.edu.sg>
2017-11-11 23:20:45 +0800

    Make some fixes and pushed to server

--- bookTicket.php
+++ bookTicket.php
@@ -7,7 +7,7 @@
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
     </head>
-    <body onLoad="backButtonOverrideBody()">
+    <body>
         <script src="js/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script src="js/scripts.js"></script>
